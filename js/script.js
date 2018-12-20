47a9763c204d766d6d7e05c571544f2b1bcbd638
Tytarenko Stanislav <civictyper13@gmail.com>
2018-12-20 22:16:28 +0200

    Small styling for all sections

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@ $(document).ready(function(){
         asNavFor: '.slider-for',
         dots: false,
         focusOnSelect: true,
-        // centerMode: true,
-        // variableWidth: true
+        centerMode: true,
+        variableWidth: true
     });
     // Init Slider End //
 
@@ -35,7 +35,7 @@ $(document).ready(function(){
     for (let tab of serviceTabs) {
         tab.setAttribute("data-tab", i);
         tab.addEventListener('click', function() {
-            deleteActiveTabs();
+            deleteActiveTabs(serviceTabs);
             this.classList.add('active');
             hideAllContents();
             serviceContents[tab.dataset.tab].classList.remove('not-active');
@@ -43,12 +43,6 @@ $(document).ready(function(){
         })
         i++;
     };
-
-    function deleteActiveTabs() {
-        for (let tab of serviceTabs) {
-            tab.classList.remove('active');
-        }
-    }
 
     function hideAllContents() {
         for (let content of serviceContents) {
@@ -59,10 +53,135 @@ $(document).ready(function(){
     // Custom Tabs End //
 
 
+    // Amazing Work Logic Start //
+
+    // Initial statement for tabs and work contents
+    const TAB_ALL_WORK = 'all';
+    const LIMIT_WORK_ITEMS = 4;
+
+    let workTabs = document.getElementById('js-work-tabs-list').getElementsByTagName("li");
+    let workContents = document.getElementsByClassName('js-item-work');
+    let btnLoad = document.getElementById('js-btn-load-more');
+
+    workTabs[0].classList.add('active');
+
+    let visibleWorks = [];
+    for (let elem of workContents) {
+        if (visibleWorks.length < LIMIT_WORK_ITEMS) {
+            visibleWorks.push(elem);
+            elem.classList.add('active');
+        } else {
+            elem.classList.add('not-active');
+        }
+    }
+
+    if (workContents.length > LIMIT_WORK_ITEMS) {
+        btnLoad.style.display = 'inline-block';
+    } else {
+        btnLoad.style.display = 'none';
+    }
+
+    // Tab click
+    let j = 0;
+    for (let tab of workTabs) {
+        tab.addEventListener('click', function() {
+            deleteActiveTabs(workTabs);
+            this.classList.add('active');
+            let fw = this.getAttribute('data-tab-work');
+            filterWorks(fw);
+
+        })
+        j++;
+    };
+
+    // Filter logic
+    function filterWorks(param) {
+        let collectionOfWorks = document.getElementsByClassName('js-item-work');
+
+        let visibleWorks = [];
+        let notVisibleWorks = [];
+
+        for (let elem of collectionOfWorks) {
+
+            elem.classList.remove('not-active');
+            elem.classList.remove('active');
+
+            if (param === TAB_ALL_WORK || elem.getAttribute('data-work') === param) {
+                if (visibleWorks.length < LIMIT_WORK_ITEMS) {
+                    visibleWorks.push(elem);
+                    // notVisibleWorks.push(elem);
+                    elem.classList.add('active');
+                } else {
+                    elem.classList.add('not-active');
+                }
+            } else {
+                elem.classList.add('not-active');
+                elem.classList.remove('active');
+            }
+
+
+            //
+            // if (param === TAB_ALL_WORK) {
+            //
+            //     if (visibleWorks.length <= LIMIT_WORK_ITEMS - 1) {
+            //         visibleWorks.push(elem);
+            //         elem.classList.add('active');
+            //     } else {
+            //         elem.classList.add('not-active');
+            //     }
+            //
+            //     // elem.classList.remove('not-active');
+            //     // elem.classList.add('active');
+            // } else if (elem.getAttribute('data-work') === param) {
+            //
+            //     if (visibleWorks.length <= LIMIT_WORK_ITEMS - 1) {
+            //         visibleWorks.push(elem);
+            //         elem.classList.add('active');
+            //     } else {
+            //         elem.classList.add('not-active');
+            //     }
+            //
+            //     // elem.classList.remove('not-active');
+            //     // elem.classList.add('active');
+            // } else {
+            //     elem.classList.add('not-active');
+            //     elem.classList.remove('active');
+            //     // elem.style.display = 'none';
+            // }
+
+
+        }
+
+        console.log(visibleWorks.length);
+
+
+        if (visibleWorks.length > LIMIT_WORK_ITEMS) {
+            btnLoad.style.display = 'inline-block';
+            console.log('block');
+        } else {
+            btnLoad.style.display = 'none';
+            console.log('none');
+        }
+
+    }
+
+
+    // Amazing Work Logic End //
+
+
     // Current Year in Footer Start //
     let fullDate = new Date();
     let currYear = document.getElementById('js-current-year');
     currYear.innerText = fullDate.getFullYear();
     // Current Year in Footer End //
+
+
+    // Global Functions Start //
+    function deleteActiveTabs(param) {
+        for (let tab of param) {
+            tab.classList.remove('active');
+        }
+    }
+    // Global Functions End //
 
 });
